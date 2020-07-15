1cf91eccb029785686929c155ae150a2b923bdf4
Md Emran Nazir <mdemrannazir9999@gmail.com>
2020-07-15 08:48:46 +0600

    basicJavascript by Programming Hero

---
+++ basicJavascript.js
@@ -0,0 +1,120 @@
+// ..... variable diclear
+// var potetoPrice = 120;
+// console.log(potetoPrice);
+
+// var resutlMark = 1220;
+// console.log(typeof resutlMark);
+
+// var name = 'Emran Nazir'
+// console.log(typeof name);
+
+// var isHot = true
+// var isRich = false
+
+// console.log(isHot);
+// console.log(typeof isRich);
+
+
+//..........String
+
+// var promise = "I promise i will work hard to become a programar";
+// console.log(promise.toLocaleUpperCase());
+// console.log(promise.toLowerCase());
+// console.log(promise.indexOf('will'));
+// console.log(promise.split(' '));
+
+// var str = 'It iS a 5r&e@@t Day.'
+// var array = str.split(" ",4);
+// console.log(array);
+
+// integer flot parseInt parseFlat type conversion
+
+// var number1 = 21;
+// var number2 = '2.2';
+
+//     // number2 = parseFloat(number2)
+//     // number2 = parseInt(number2)
+//     // number1 = '' + number1
+
+// console.log(number1 + number2);
+
+// var number1 = 0.1;
+// var number2 = 0.2;
+// // console.log(number1 + number2);
+
+// var total = number1 + number2;
+// total = total.toFixed(1)
+// console.log(total);
+
+// mathematical Operation
+
+// var a = 10;
+// var b = 30;
+
+// // var z = a + b;
+// // var z = a - b;
+// // var z = a * b;
+// // var z =  b / a;
+// var z = a % b;
+// console.log(z);
+
+// var name = 'emran';
+// var roll = 328435;
+// var result = name + roll
+// console.log(result);
+
+// var firstName = 'Emran';
+// var listName = 'Nazir';
+// var fullName = firstName + ' ' + listName;
+// console.log(fullName);
+
+// Math absolute round floor ceil random
+
+// var number = +6
+// var absoluteNumber = Math.abs(number)
+// console.log(absoluteNumber);
+
+// var number = 6.400
+// var roundNumber = Math.round(number)
+// console.log(roundNumber);
+
+// var number = 6.9900
+// var floorNumber = Math.floor(number)
+// console.log(floorNumber);
+
+// var number = 6.400
+// var ceilNumber = Math.ceil(number)
+// console.log(ceilNumber);
+
+// var number = Math.random() * 100;
+// var result = Math.round(number)
+// console.log(result);
+
+// condictional statement
+
+// var roll = 20
+// if (roll < 20) {
+//     console.log('JHPI Student');
+// } else {
+//     console.log('Not JHPI Student');
+// }
+
+// multipul condiction
+
+// var jobPaico = true;
+// var savingAmount = 50000;
+
+// if (jobPaico == true && savingAmount > 70000) {
+//     console.log('cholo parti khujte jai.');
+// } else if (jobPaico == false || savingAmount > 20000) {
+//     console.log('tomar prem hobe but biya hobe na');
+// } else {
+//     console.log('tor kopale prem and biya kichui nai');
+// }
+
+// Date TimeZone
+
+var date = new Date('2001-10-01')
+console.log(date);
+
+
